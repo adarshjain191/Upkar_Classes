9a2ac8da0d32aee6310c6857be2f7543def2f16a
aadarsh jain <adarshjain191@gamil.com>
2022-03-17 18:18:14 +0530

    some changes

--- main/views.py
+++ main/views.py
@@ -3,8 +3,8 @@ from django.http import HttpResponse
 from main.models import Contact
 from django.contrib import messages
 
-
 def home(request):
+    # l=["I have a template songs.html that includes a child template addToPlaylist.html. I need the title of the song that is to be added to the playlist dynamically. Here is the code for songs.html which includes addToPlaylist.html.","@Moosa I want the value of song.title that I am iterating in parent. So I can add relevant song to the playlist user chooses. song.title should be the title of song for which addToPlaylist button is clicked."]
     return render(request,"Home.html")
 def AboutUs(request):
     return render(request,"About.html")
